1d8f8fb9bd7414d9d3fc7dc252f3ebb16d32578b
amgranad <ambergkim@gmail.com>
2018-01-06 17:20:20 -0800

    mvp

--- server.js
+++ server.js
@@ -17,15 +17,30 @@ client.on('error', err => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
-app.get('/test', (req, res) => {
-  client.query(`SELECT * FROM books;`)
+app.get('/', (request, response) => response.sendFile('index.html', {root: ''}));
+
+app.get('/api/v1/books', (req, res) => {
+  client.query(`SELECT book_id, title, author, image_url FROM books;`)
     .then(data => {
-      res.send(data);
+      res.send(data.rows);
     }).catch(err => {
       console.err(err);
     });
   //res.send();
 });
+  
+
+
+// app.get('/test', (req, res) => {
+//   client.query(`SELECT * FROM books;`)
+//     .then(data => {
+//       res.send(data);
+//     }).catch(err => {
+//       console.err(err);
+//     });
+//   //res.send();
+// });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
