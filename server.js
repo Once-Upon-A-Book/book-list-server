a1f5705b403d323a17f85e8b26428bba59b7a342
alicialycan <alicialycan@gmail.com>
2018-01-08 21:42:48 -0800

    debug

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const conString = 'postgres://alicialycan:534@localhost:5432/books_app';
-const conString = 'postgres://amgranad:amber123@localhost:5432/books_app';
+const conString = 'postgres://alicialycan:534@localhost:5432/books_app';
+// const conString = 'postgres://amgranad:amber123@localhost:5432/books_app';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => {
@@ -19,8 +19,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-app.get('/', (request, response) => response.sendFile('index.html', {root: ''}));
-
 app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url FROM books;`)
     .then(data => {
@@ -30,8 +28,6 @@ app.get('/api/v1/books', (req, res) => {
     });
   //res.send();
 });
-  
-
 
 // app.get('/test', (req, res) => {
 //   client.query(`SELECT * FROM books;`)
