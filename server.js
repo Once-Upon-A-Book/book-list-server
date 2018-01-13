08a477928d5373d53ae1ee59a67e9e0cdc0c6f70
alicialycan <alicialycan@gmail.com>
2018-01-13 14:11:51 -0800

    update delete button functionality

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const DATABASE_URL = process.env.DATABAE_URL || 'postgres://alicialycan:534@localhost:5432/books_app';
-//const DATABASE_URL = process.env.DATABAE_URL || 'postgres://amgranad:amber123@localhost:5432/books_app';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://alicialycan:534@localhost:5432/books_app';
+//const DATABASE_URL = process.env.DATABASE_URL || 'postgres://amgranad:amber123@localhost:5432/books_app';
 
 const client = new pg.Client(DATABASE_URL);
 client.connect();
@@ -34,9 +34,7 @@ app.get('/api/v1/books/:id', (req, res) => {
   client.query(`
     SELECT * FROM books WHERE book_id=$1;
   `, [req.params.id]
-  ).then(result => {
-    res.send(result.rows[0]);
-  })
+  ).then(result => res.send(result.rows[0]))
     .catch(err => console.error(err));
 });
 
@@ -55,5 +53,16 @@ app.post('/api/v1/books', (req, res) => {
   ).then(() => res.send('inserted successfully'))
     .catch(err => console.error(err));
 });
+
+app.delete('/api/v1/books/:id', (req, res) => {
+  client.query(`
+  DELETE FROM books WHERE book_id=$1;  
+  `,
+    [req.params.id]
+  ).then(result => res.send(result))
+    .catch(err => console.error(err));
+});
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
